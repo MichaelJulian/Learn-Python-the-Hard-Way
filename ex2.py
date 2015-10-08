dc4604364635144af70ba5d2f84b2b334232afef
MichaelJulian <mikeyjmikeyj@gmail.com>
2015-10-08 16:08:35 -0700

    Do first 6 exercises

---
+++ ex2.py
@@ -0,0 +1,6 @@
+# A comment
+
+print "I Could have code like this." # w/ comment after
+# print "This won't run."
+
+print "This will run."
---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Michael Julian'
+my_age = 21
+my_height = 72
+my_weight = 160
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s." % my_teeth
+
+print "If I add %d, %d, and %d, I get %d." % (
+my_age, my_height, my_weight, my_age + my_height + my_weight)

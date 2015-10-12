7ad4576883a249e6dde88a4b19bc4f7377a57141
MichaelJulian <mikeyjmikeyj@gmail.com>
2015-10-12 12:22:25 -0700

    Exercises 7-12

---
+++ ex11.py
@@ -0,0 +1,12 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "%r %r %r" % (age, height, weight)
+
+x = raw_input('Another way to use raw_input. enter something')
+
+print x
